9b02e89fa66aa046cd49f0d17f86ee3cbc0ced05
chrisjc9931 <>
2021-06-07 01:16:04 +0000

    ,,

--- index.js
+++ index.js
@@ -32,3 +32,41 @@ let isValidPassword = function(password) {
 };
 
 console.log(getValidInput('Create a password:', isValidPassword));
+
+let isInputA = function(inputA){
+  if(inputA.indexOf(`a`) === 0 || inputA.indexOf(`A`) === 0){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(getValidInput('Enter a word beinging with "a": ', isInputA));
+
+let isInputVowel = function(inputV){
+  let vowels = ['a','e','i','o','u','y'];
+  //for(let i = 0; i < vowels.length; i++){
+    if(vowels.includes(inputV[0])){
+      return true;
+    } else {
+      return false;
+    }
+  //} 
+};
+
+console.log(getValidInput('Enter a word beinging with a vowel: ', isInputVowel));
+
+
+
+let fileLogger = function(msg) {
+
+   // Put the message in a file
+
+}
+
+function logError(msg, logger) {
+   let errorMsg = 'ERROR: ' + msg;
+   logger(errorMsg);
+}
+
+logError('Something broke!', fileLogger);
